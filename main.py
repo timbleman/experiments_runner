ab49531dce98b4597cebd79ab1f67c8387925e9b
Tim Fraunholz <fraunholz.t@web.de>
2020-06-12 21:48:20 +0200

    added some code for comparison inside a batch

--- main.py
+++ main.py
@@ -5,20 +5,22 @@ from pathlib import Path
 from beamngpy import BeamNGpy, Scenario, Vehicle
 
 from coverage_collector import CoverageCollector
+from suite_behaviour_computer import SuiteBehaviourComputer
 import utils
 bng = BeamNGpy('localhost', 64256)
 
 # can only run a suite of test cases with the same name and a number at the moment
-FIRST_TEST = 97
-LAST_TEST = 99
+SCENARIO_BASE_NAME = 'road_'
+FIRST_TEST = 1
+LAST_TEST = 4
 
 tests_dict = {}
 
 vehicle = Vehicle('ego', model='etk800', licence='PYTHON')
 
-for i in range(FIRST_TEST, LAST_TEST):
+for i in range(FIRST_TEST, LAST_TEST+1):
     # Load different scenarios.
-    scenario_name = 'drivebuild_' + str(i)
+    scenario_name = SCENARIO_BASE_NAME + str(i)
     scenario = Scenario('drivebuild', scenario_name)
     scenario.add_vehicle(vehicle, pos=(0, 0, 0), rot=(0, 0, 0))
 
@@ -33,15 +35,15 @@ for i in range(FIRST_TEST, LAST_TEST):
     bng.load_scenario(scenario)
     bng.start_scenario()
 
-    #vehicle.ai_drive_in_lane(True)
-    #vehicle.ai_set_speed(20, "limit")
-    vehicle.ai_set_mode("disabled")
+    vehicle.ai_drive_in_lane(True)
+    vehicle.ai_set_speed(20, "limit")
+    vehicle.ai_set_mode("span")
 
     # Data collecting loop. Collects every three steps data.
     counter = 0
     last_counter = 0
     steps = 2
-    while counter < 100000:
+    while counter < 100:
         if counter > last_counter + steps:
             cov_collector.collect()
         counter += 1
@@ -51,13 +53,19 @@ for i in range(FIRST_TEST, LAST_TEST):
     # bng.restart_scenario()
     bng.close()
     # adds binned behavior to dict of road
-    coverage = {'steering': cov_collector.get_steering_bins(), 'throttle': cov_collector.get_throttle_bins(),
-                'speed_steering': cov_collector.get_speed_steering_2d()}
-
+    coverage = {'cov_collector': cov_collector, 'steering': cov_collector.get_steering_bins(),
+                'throttle': cov_collector.get_throttle_bins(), 'speed_steering': cov_collector.get_speed_steering_2d()}
+    cov_collector.get_speed_steering_2d()  # remove
     # print("entropy steering: ", utils.entropy_compute_1d(coverage['steering']))
     # print("2d diff: ", utils.bin_difference_2d(coverage['speed_steering'], coverage['speed_steering'], 'binary', False))
     # adds the dictionary of the current road to the global one
     tests_dict[str(i)] = coverage
 
-
+print("tests_dict: ", tests_dict)
+suitebh = SuiteBehaviourComputer(tests_dict, FIRST_TEST, LAST_TEST)
+suitebh.calculate_suite_speed_steering_coverage()
+suitebh.road_compare_1d(str(FIRST_TEST), 'steering')
+# for i in range(FIRST_TEST, LAST_TEST):
+#    coverage_dict = tests_dict[str(i)]
+#    print(i, "achieved 2d coverage: ", coverage_dict['cov_collector'])
 # print(utils.list_difference_1d([2,6,7], [1,6,8], 'squared', normalized=True))
---
+++ suite_behaviour_computer.py
@@ -0,0 +1,34 @@
+from typing import Dict
+import numpy as np
+
+import coverage_collector
+import utils
+
+NUM_BINS = coverage_collector.NUM_BINS
+
+
+class SuiteBehaviourComputer:
+    def __init__(self, t_dict: Dict, start: int, end: int):
+        self.test_dict = t_dict
+        self.start = start
+        self.end = end
+
+    def calculate_suite_speed_steering_coverage(self):
+        global_speed_steering_cov = np.zeros((NUM_BINS, NUM_BINS))
+        for i in range(self.start, self.end + 1):
+            cov_col = self.test_dict.get(str(i)).get('cov_collector')
+            global_speed_steering_cov = np.add(global_speed_steering_cov, cov_col.get_speed_steering_2d())
+        self.test_dict['whole_suite_speed_steering_coverage'] = utils.coverage_compute_2d(global_speed_steering_cov)
+        self.test_dict['whole_suite_speed_steering_entropy'] = utils.entropy_compute_2d(global_speed_steering_cov)
+        print("The suite covered: ", self.test_dict['whole_suite_speed_steering_coverage'])
+
+    def road_compare_1d(self, road_to_compare: str, measure: str):
+        road_similarities = {}
+        main_bin = self.test_dict.get(road_to_compare).get(measure)
+        assert main_bin is not None, "The bin " + measure + " has not been added or spelling is incorrect"
+        print(main_bin)
+        for i in range(self.start, self.end + 1):
+            road_similarities[str(i)] = utils.list_difference_1d(main_bin,
+                                                                 self.test_dict.get(str(i)).get(measure),
+                                                                 function='binary', normalized=True)
+        print(road_similarities)
